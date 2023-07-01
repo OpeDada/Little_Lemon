0109a508607d5e30bf73a4c64c6ef7d9e9ed51a5
OpeDada <opedada@gmail.com>
2023-07-01 10:28:54 +0900

    add the navigation and footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,63 @@
+import logo from "../assets/logo.svg";
 
 export default function Footer() {
-return (
-  <footer></footer>
-)
+  return (
+    <footer>
+      <img src={logo} alt="logo" />{" "}
+      <div className="doormat-nav">
+        <ul>
+          <p>Doormat Navigation</p>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="/order">Order Online</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+      </div>
+      <div className="contact">
+        <ul>
+          <p>Contact</p>
+          <li>
+            <a href="/">Address</a>
+          </li>
+          <li>
+            <a href="/about">Phone number</a>
+          </li>
+          <li>
+            <a href="/menu">Email</a>
+          </li>
+        </ul>
+      </div>
+      <div className="socials">
+        <ul>
+          <p>Social Media Links</p>
+          <li>
+            <a href="/">Facebook</a>
+          </li>
+          <li>
+            <a href="/about">Linkedin</a>
+          </li>
+          <li>
+            <a href="/menu">Instagram</a>
+          </li>
+        </ul>
+      </div>
+      <p>
+        &copy; {new Date().getFullYear()} Little Lemon. All rights reserved.
+      </p>
+    </footer>
+  );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,3 +1,31 @@
+import logo from "../assets/logo.svg";
+
 export default function Nav() {
-  return <nav></nav>;
+  return (
+    <>
+      <img src={logo} alt="logo" />
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/menu">Menu</a>
+          </li>
+          <li>
+            <a href="/reservations">Reservations</a>
+          </li>
+          <li>
+            <a href="/order">Order Online</a>
+          </li>
+          <li>
+            <a href="/login">Login</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
