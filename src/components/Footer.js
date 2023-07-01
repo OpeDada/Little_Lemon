fd807845cde757d16185f80eda3e586bfc122ae3
OpeDada <opedada@gmail.com>
2023-07-01 22:07:00 +0900

    turn footer section into grid

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,58 +3,53 @@ import logo from "../assets/logo.svg";
 export default function Footer() {
   return (
     <footer>
-      <img src={logo} alt="logo" />{" "}
-      <div className="doormat-nav">
-        <ul>
-          <p>Doormat Navigation</p>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/menu">Menu</a>
-          </li>
-          <li>
-            <a href="/reservations">Reservations</a>
-          </li>
-          <li>
-            <a href="/order">Order Online</a>
-          </li>
-          <li>
-            <a href="/login">Login</a>
-          </li>
-        </ul>
-      </div>
-      <div className="contact">
-        <ul>
-          <p>Contact</p>
-          <li>
-            <a href="/">Address</a>
-          </li>
-          <li>
-            <a href="/about">Phone number</a>
-          </li>
-          <li>
-            <a href="/menu">Email</a>
-          </li>
-        </ul>
-      </div>
-      <div className="socials">
-        <ul>
-          <p>Social Media Links</p>
-          <li>
-            <a href="/">Facebook</a>
-          </li>
-          <li>
-            <a href="/about">Linkedin</a>
-          </li>
-          <li>
-            <a href="/menu">Instagram</a>
-          </li>
-        </ul>
-      </div>
+      <img src={logo} alt="logo" />
+      <ul className="footer-links">
+        <p>Doormat Navigation</p>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="/order">Order Online</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+      <ul className="footer-links">
+        <p>Contact</p>
+        <li>
+          <a href="/">Address</a>
+        </li>
+        <li>
+          <a href="/about">Phone number</a>
+        </li>
+        <li>
+          <a href="/menu">Email</a>
+        </li>
+      </ul>
+      <ul className="footer-links">
+        <p>Social Media Links</p>
+        <li>
+          <a href="/">Facebook</a>
+        </li>
+        <li>
+          <a href="/about">Linkedin</a>
+        </li>
+        <li>
+          <a href="/menu">Instagram</a>
+        </li>
+      </ul>
+
       <p>
         &copy; {new Date().getFullYear()} Little Lemon. All rights reserved.
       </p>
